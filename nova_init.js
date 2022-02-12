80a9ad4fa7e9e5d75d1958b1f5a42f209ed81db0
attanyl <attanayal01@gmail.com>
2022-02-11 21:18:49 -0500

    Added javascript code stuff!

---
+++ nova_init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAScu_-xjhfkbmDqjSmO71zSIhU-j1tlD0",
+    authDomain: "nova-f2979.firebaseapp.com",
+    projectId: "nova-f2979",
+    storageBucket: "nova-f2979.appspot.com",
+    messagingSenderId: "490015709518",
+    appId: "1:490015709518:web:e87ba1ea9b98e810a8a61f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
