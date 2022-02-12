79197edc3e051f6f98463dbb50088d8c7c27f42b
davidhabboosh1 <habboosh.d@northeastern.edu>
2022-02-12 12:19:25 -0500

    STUFF IS GOOD!

--- nova-firebase/public/index.js
+++ nova-firebase/public/index.js
@@ -1,8 +1,8 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js';
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js"
+import * as admin from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 
-const firebaseConfig = {
+const app = admin.initializeApp({
     apiKey: "AIzaSyAScu_-xjhfkbmDqjSmO71zSIhU-j1tlD0",
     authDomain: "nova-f2979.firebaseapp.com",
     databaseURL: "https://nova-f2979-default-rtdb.firebaseio.com",
@@ -10,38 +10,9 @@ const firebaseConfig = {
     storageBucket: "nova-f2979.appspot.com",
     messagingSenderId: "490015709518",
     appId: "1:490015709518:web:e87ba1ea9b98e810a8a61f"
-};
-
-const app = initializeApp(firebaseConfig);
+});
 
 // Get a reference to the database service
 const database = getDatabase(app);
-
-function login(){
-
-    let userEmail = document.getElementById("email_field").value;
-    let userPass = document.getElementById("password_field").value;
-
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-        let errorMessage = error.message;
-
-        window.alert("Error : " + errorMessage);
-    });
-
-}
-
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    console.log(user);
-    console.log(uid);
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
 
 console.log(database);
