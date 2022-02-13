8259c7f94fe4184967bd4e8159b0f36187afd58a
davidhabboosh1 <habboosh.d@northeastern.edu>
2022-02-12 19:26:49 -0500

    fixed.

--- nova-firebase/public/index.js
+++ nova-firebase/public/index.js
@@ -1,14 +1,10 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js';
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js";
-import { collection, addDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
-import { getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
-import { getFirestore } from "firebase/firestore"
-import 'firebase/firestore';
-import 'firebase/auth';
+import { collection, setDoc, getFirestore } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js"
+import {getAuth} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js"
 
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+// const firebase = require("firebase");
+// // Required for side-effects
+// require("firebase/firestore");
 
 const firebaseConfig = {
     apiKey: "AIzaSyAScu_-xjhfkbmDqjSmO71zSIhU-j1tlD0",
@@ -20,7 +16,7 @@ const firebaseConfig = {
     appId: "1:490015709518:web:e87ba1ea9b98e810a8a61f",
 };
 
-const app = firebase.initializeApp({
+const app = initializeApp({
     apiKey: "AIzaSyAScu_-xjhfkbmDqjSmO71zSIhU-j1tlD0",
     authDomain: "nova-f2979.firebaseapp.com",
     databaseURL: "https://nova-f2979-default-rtdb.firebaseio.com",
@@ -31,14 +27,14 @@ const app = firebase.initializeApp({
 })
 
 // Get a reference to the database service
-const db = firebase.getFirestore();
+const db = getFirestore();
 
 function login(){
 
     let userEmail = document.getElementById("email_field").value;
     let userPass = document.getElementById("password_field").value;
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    getAuth().signInWithEmailAndPassword(userEmail, userPass)
     .then(async (userCredential) => {
         const user = userCredential.user;
         console.log(user);
@@ -68,11 +64,11 @@ function login(){
 
 }
 
-firebase.auth().onAuthStateChanged((user) => {
+getAuth().onAuthStateChanged((user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
+    let uid = user.uid;
     console.log(user);
     console.log(uid);
     // ...
